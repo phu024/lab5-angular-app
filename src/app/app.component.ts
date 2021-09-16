d2ea6b3a4bbceade5439b3cf2cd03cadd89de718
phu024 <phuwadon.dev@gmail.com>
2021-09-17 00:54:47 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  parentColorProperty : string='';
+
+  receiveData($event: string){
+    this.parentColorProperty=$event;
+  }
+
+  ngOnInit(): void {
+  }
+
   title = 'lab5-angular-app';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgclassComponent } from './components/ngclass/ngclass.component';
+import { NgstyleComponent } from './components/ngstyle/ngstyle.component';
+import { NgifComponent } from './components/ngif/ngif.component';
+import { NgswitchComponent } from './components/ngswitch/ngswitch.component';
+import { NgforComponent } from './components/ngfor/ngfor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgclassComponent,
+    NgstyleComponent,
+    NgifComponent,
+    NgswitchComponent,
+    NgforComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
